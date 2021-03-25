95fd40ff55af634da696f226a3471c3e18ebd753
dirtboll <dirtboll.boi@gmail.com>
2021-03-25 08:13:04 +0700

    OS211 dirtboll

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,10 +12,10 @@ for II in W?? ; do
     TARFASC=$TARFILE.asc
     rm -f $TARFILE $TARFASC
     echo -e "\e[36mCreating tar from $II\e[0m"
-    echo -e "\e[33;1m> tar cfj $TARFILE $II/\e[0m"
+    echo -e "\e[33;1m> tar cfj $TARFILE $II/\e\n[0m"
     tar cfj $TARFILE $II/
     echo -e "\e[36mSigning $TARFILE\e[0m"
-    echo -e "\e[33;1m> gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE\e[0m"
+    echo -e "\e[33;1m> gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE\e\n[0m"
     gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
 done
 popd
@@ -26,7 +26,7 @@ for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
    [ -f $II ] && mv -f $II .
 done
 
-echo -e "\e[36mDeleting previous files\n\e[33;1m> rm -f $SHA $SHA.asc\e[0m"
+echo -e "\n\e[36mDeleting previous files\n\e[33;1m> rm -f $SHA $SHA.asc\e[0m"
 rm -f $SHA $SHA.asc
 
 echo -e "\n\e[36mGenerating checksum\n\e[33;1m> sha256sum $FILES > $SHA\e[0m"
